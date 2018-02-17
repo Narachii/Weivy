1dde4becd98a1132a841d5107844d5d8783e4434
Narachii <naoya706oxford@me.com>
2018-02-18 02:35:07 +0900

    ログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :authentication_keys[],
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :authentication_keys => [:nickname]
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions).where(["nickname = :value", { :value => nickname }]).first
+      else
+        where(conditions).first
+      end
+  end
+
+  def email_required?
+    false
+  end
+  def email_changed?
+    false
+  end
 end
