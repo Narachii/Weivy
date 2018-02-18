88dc28480ec5459956732222e533cfc8e98d6229
kogriffey <kogriffey@kogriffey-no-MacBook-Pro.local>
2018-02-18 21:40:34 +0900

    facebook ログインをしゅうせi

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  devise_scope :user do
-    get "sign_in", :to => "users/sessions#new"
-    get "sign_out", :to => "users/sessions#destroy"
-  end
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: "statics#index"
 
