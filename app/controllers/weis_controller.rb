42c061757a7ef7ca97c196569e97ec743f3fabe2
Naoya Nara <naoya706oxford@me.com>
2018-02-18 13:43:04 +0900

    wei成立機能の追加

--- app/controllers/weis_controller.rb
+++ app/controllers/weis_controller.rb
@@ -6,13 +6,18 @@ class WeisController < ApplicationController
   end
 
   def create
-    @wei = Wei.new(wei_params)
-    @wei.save
+    wei = Wei.new(wei_params)
+    wei.save
+  end
+
+  def update
+    wei = Wei.find(params[:id])
+    wei.update(wei_params)
   end
 
   private
 
   def wei_params
-    params.require(:wei).permit(:sender, :receiver)
+    params.require(:wei).permit(:sender, :receiver, :wei)
   end
 end
