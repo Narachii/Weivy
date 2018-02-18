af1a5d6c6ae90b11194a8395e2527596b3d44784
Hideo Takeuchi <h.takeuchi@bizcast.bz>
2018-02-18 20:04:07 +0900

    add relation

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   resources :users
   resources :weis
   resources :statics
+  resources :user_locations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
