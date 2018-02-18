b71781b45568fc64b57c6bbad19fc180a3065aad
Hideo Takeuchi <h.takeuchi@bizcast.bz>
2018-02-18 14:43:24 +0900

    add geolocation

---
+++ app/assets/javascripts/geolocation.js
@@ -0,0 +1,7 @@
+function hoge() {
+  if (navigator.geolocation) {
+    console.log("OK")
+  } else {
+    console.log("NG")
+  }
+}
--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180218025850) do
+ActiveRecord::Schema.define(version: 20180218051952) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "nickname",                            null: false
-    t.integer  "sex",                    default: 0,  null: false
+    t.string   "nickname",                                       null: false
+    t.integer  "sex",                               default: 0,  null: false
     t.datetime "last_wei"
     t.boolean  "is_weiting"
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "encrypted_password",                default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                     default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                     null: false
+    t.datetime "updated_at",                                     null: false
+    t.string   "address"
+    t.float    "latitude",               limit: 24
+    t.float    "longitude",              limit: 24
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
